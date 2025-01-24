6c7d0e916844d3abcfe0f28d2d3f786f3e2e57ab
Usuario_Tarde <Usuario_Tarde@ALUMNO12B3>
2025-01-24 16:52:21 +0100

    Agregar archivos de proyecto.

---
+++ HugoNeiraMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HugoNeiraMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HugoNeiraMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HugoNeiraMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
